1218096d11cd109af39541a5aa5c07c270b09d10
harshBansal47 <harshbansal472000@gmail.com>
2024-09-06 11:49:49 +0530

    commit

--- config/index.js
+++ config/index.js
@@ -1,14 +1,22 @@
 const mongoose = require('mongoose');
-require('dotenv').config(); 
-
+require('dotenv').config();
+const detectEnvironment = require('../utils/detectenv');
 
 const connectDB = async () => {
   try {
-    const db_url = process.env.DATABASE_URL || "http://localhost:27017/proptrade";
-    const conn = await mongoose.connect(db_url);
+    const env = await detectEnvironment(); // Wait for environment detection
+    console.log('Detected environment:', env);
+
+    if (env === 'local') {
+      process.env.DATABASE_URL = "mongodb://localhost:27017/proptrade"; // Set local DB URL
+    }
+
+    const db_url = process.env.DATABASE_URL;
+    const conn = await mongoose.connect(db_url); // Await the DB connection
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
+    console.error(`Failed to detect environment or connect to DB: ${error.message}`);
     process.exit(1); // Exit process with failure
   }
 };
